82a75f8c4e631f4ea3889b1742cc5e1048770c45
chinmay <chinmaymhatre12@gmail.com>
2021-11-03 14:14:13 +0530

    add basic fields

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text>Name: {name}</Text>
+        <Text>Age : {age}</Text>
+        <Text>Email : {email}</Text>
+        <Text>Password : {password}</Text>
+      </View>
+      <View style={styles.container}>
+        <TextInput 
+        style = {styles.input}
+        placeholder="Name" 
+        value={name}
+        onChangeText={(text)=>setName(text)}
+        />
+        <TextInput 
+        style = {styles.input}
+        placeholder="Age"
+        value={age}  
+        keyboardType="number-pad"
+        onChangeText={(text)=>setAge(text)}
+        />
+        <TextInput 
+        style = {styles.input}
+        placeholder="Email"
+        value={email}  
+        keyboardType="email-address"
+        onChangeText={(text)=>setEmail(text)}
+        />
+        <TextInput 
+        style = {styles.input}
+        placeholder="Password"
+        value={password}  
+        secureTextEntry={true}
+        textContentType="newPassword"
+        onChangeText={(text)=>setPassword(text)}
+        />
+      </View>
+      
     </View>
   );
 }
@@ -17,5 +57,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width:"100%"
+  },
+  input: {
+    height: 40,
+    width:"80%",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
